a8b5ae47b2a463edfee9b6f74231dd0f58b05042
Roberto Damico <rdamico.reply@corteconti.it>
2017-08-30 12:34:48 +0200

    rimosso codice sbagliato

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,3 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-@Component
-({ 
-  selector: 'hello-angular', 
-  template: '<h1> {{greeting}} </h1>' 
-}) 
-class HelloAngularComponent { 
-  greeting: string; 
-  constructor() 
-  { 
-    this.greeting = 'Hello Angular 2!'; 
-  } 
-}
